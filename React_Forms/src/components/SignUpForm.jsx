e9ae0af560b1f29b67b445c3fac7721e07e0b1ef
Alberto Martinez <avmrbeto98@gmail.com>
2024-05-15 19:44:18 -0700

    Sign Up components completed

---
+++ React_Forms/src/components/SignUpForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function SignUpForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+    
+        try {
+            const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/signup`)
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+          setError(error.message);
+        }
+      }
+
+    return (
+        <div>
+            <h2>Sign Up!</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+                </label>
+                <label>
+                    Password: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+
+    )
+
+}
